3481dd823249db0c28841248c58e2905976a92ce
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-01-27 16:19:01 +0300

    Найти класс по описанию. Старт задачи

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3703/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3703/Solution.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ConcurrentSkipListMap;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(getExpectedClass());
+        System.out.println("Старт задачи");
     }
 
     public static Class getExpectedClass() {
