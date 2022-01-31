9cd3ab7ec5d52f55185b8de858e627977651ec0a
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-01-31 19:34:48 +0300

    Решена очередная задача

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3703/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3703/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task37.task3703;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 /* 
@@ -7,9 +9,53 @@ import java.util.concurrent.ConcurrentSkipListMap;
 */
 
 public class Solution {
+    Map m = new HashMap();
+
     public static void main(String[] args) {
-        System.out.println(getExpectedClass());
+        System.out.println("Тестирование разных Map");
+
+        Solution s = new Solution();
+        s.go();
+
     }
+
+    public Solution () {
+    }
+
+    public void go () {
+        ThreadAdd ta = new ThreadAdd();
+        ta.start();
+
+        ThreadGet tg = new ThreadGet();
+        tg.start();
+
+    }
+
+    public class ThreadAdd extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Я поток добавления!");
+            for (int i = 0; i < 10000000; i++) {
+                m.put(i,i);
+            }
+            System.out.println("Все элементы добавлены!");
+        }
+    }
+
+    public class ThreadGet extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Я поток взятия!");
+            for (int i = 10000000; i < 20000000; i++) {
+                m.put(i,i);
+//                m.get(i);
+            }
+            System.out.println("Все элементы взяты!");
+        }
+    }
+
+
+
 
     public static Class getExpectedClass() {
         return ConcurrentSkipListMap.class;
