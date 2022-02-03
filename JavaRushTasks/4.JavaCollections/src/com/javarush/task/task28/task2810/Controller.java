49cd8e8c6dfa41e3e50500efd5ff5662798d75ad
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-02-03 08:38:35 +0300

    Промежуточный коммит

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -0,0 +1,1 @@
+package com.javarush.task.task28.task2810;import com.javarush.task.task28.task2810.model.Provider;public class Controller {    private Provider[] providers;    public Controller(Provider... providers) {        if (providers.length == 0) throw new IllegalArgumentException();        this.providers = providers;    }}
---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/Strategy.java
@@ -0,0 +1,1 @@
+package com.javarush.task.task28.task2810.model;public interface Strategy {}
