dd0cb93eb11d98126881ab96b6b6803750601d36
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-02-09 12:38:17 +0300

    Докопал Maven. Пока хватит

--- JavaRushTasks/9.Maven/src/main/java/TestMaven.java
+++
@@ -1,9 +0,0 @@
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-
-public class TestMaven {
-    public static void main(String[] args) {
-        System.out.println("Здравствуй мир");
-    }
-}
