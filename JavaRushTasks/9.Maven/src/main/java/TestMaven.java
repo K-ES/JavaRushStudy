f016c4f538378466c995c23da2955db12f49ea85
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-02-09 07:27:31 +0300

    Кнопка Maven не активна. Серый цвет. Разбираюсь.

---
+++ JavaRushTasks/9.Maven/src/main/java/TestMaven.java
@@ -0,0 +1,9 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class TestMaven {
+    public static void main(String[] args) {
+        System.out.println("Здравствуй мир");
+    }
+}
