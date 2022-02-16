d15877a16c1c1e178db170537e885a1ce6cb3f9e
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-02-16 05:45:16 +0300

    Перед очередной мелкой

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task39/task3909/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task39/task3909/Solution.java
@@ -6,12 +6,10 @@ package com.javarush.task.task39.task3909;
 
 public class Solution {
     public static void main(String[] args) {
-        isOneEditAway("string", "strs");
+
     }
 
     public static boolean isOneEditAway(String first, String second) {
-        System.out.println("Старт выполнения");
-        boolean result = false;
-        return result;
+
     }
 }
