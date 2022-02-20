32681f28797dd1ae793d050df1c7f6758038d066
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-02-20 07:25:22 +0300

    Пощупал init и destroy

--- JavaRushTasks/A.Spring/src/main/java/ru/krivonogov/springexample/ClassicalMusic.java
+++ JavaRushTasks/A.Spring/src/main/java/ru/krivonogov/springexample/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package ru.krivonogov.springexample;
 
 public class ClassicalMusic implements Music{
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doByDestroy() {
+        System.out.println("Doing my destrcution");
+    }
+
     @Override
     public String getSong() {
         return "Венгерская рапсодия";
