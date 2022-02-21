820a732459df75b628063befee64410bbbfef05e
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-02-21 07:58:42 +0300

    Начал щупать @Autowired

--- JavaRushTasks/A.Spring/src/main/java/ru/krivonogov/springexample/ClassicalMusic.java
+++ JavaRushTasks/A.Spring/src/main/java/ru/krivonogov/springexample/ClassicalMusic.java
@@ -1,5 +1,8 @@
 package ru.krivonogov.springexample;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music{
     private ClassicalMusic() {
     }
--- JavaRushTasks/A.Spring/src/main/java/ru/krivonogov/springexample/MusicPlayer.java
+++ JavaRushTasks/A.Spring/src/main/java/ru/krivonogov/springexample/MusicPlayer.java
@@ -1,39 +1,15 @@
 package ru.krivonogov.springexample;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
 
-    private String name;
-    private int volume;
+    public MusicPlayer(ClassicalMusic classicalMusic) {        this.classicalMusic = classicalMusic;    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println(music.getSong());
-    }
+    public void playMusic() {        System.out.println(classicalMusic.getSong());    }
 }
