d6558316edfff39ad112131db35bae8ec9010525
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-02-23 10:06:37 +0300

    Дощупал Pre и Post для инициализаций

--- JavaRushTasks/A.Spring/src/main/java/ru/krivonogov/springexample/ClassicalMusic.java
+++ JavaRushTasks/A.Spring/src/main/java/ru/krivonogov/springexample/ClassicalMusic.java
@@ -1,21 +1,20 @@
 package ru.krivonogov.springexample;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class ClassicalMusic implements Music{
-    private ClassicalMusic() {
-    }
-
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
-
+    @PostConstruct
     public void doMyInit() {
         System.out.println("Doing my initialization");
     }
 
+    @PreDestroy
     public void doByDestroy() {
         System.out.println("Doing my destrcution");
     }
