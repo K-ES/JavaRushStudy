790a03733d11ba0d0705842eed68e901baa57d83
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-03-02 07:11:32 +0300

    no message

--- JavaRushTasks/SpringMVCApp1/src/main/java/ru/krivonogov/springcourse/controllers/SecondController.java
+++ JavaRushTasks/SpringMVCApp1/src/main/java/ru/krivonogov/springcourse/controllers/SecondController.java
@@ -1,6 +1,15 @@
 package ru.krivonogov.springcourse.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SecondController {
 
-    pub
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
+    }
+
 }
