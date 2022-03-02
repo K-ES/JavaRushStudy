9ff853575e06daa1662cbbd60c020a1a301e7f3f
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-03-02 07:44:59 +0300

    Завершил изучение аннотации RequestParam

--- JavaRushTasks/SpringMVCApp1/src/main/java/ru/krivonogov/springcourse/controllers/FirstController.java
+++ JavaRushTasks/SpringMVCApp1/src/main/java/ru/krivonogov/springcourse/controllers/FirstController.java
@@ -3,13 +3,19 @@ package ru.krivonogov.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name1, @RequestParam(value = "surname", required = false) String surname1) {
+
+        System.out.println("Hi, " + name1 + " " + surname1);
+
         return "first/hello";
     }
 
