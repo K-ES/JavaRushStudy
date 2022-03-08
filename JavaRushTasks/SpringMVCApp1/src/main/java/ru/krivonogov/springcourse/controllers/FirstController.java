c74826f1b5b049e494b92cabb16c1ec8beb36244
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-03-08 05:57:03 +0300

    Вернулся к разбору модели

--- JavaRushTasks/SpringMVCApp1/src/main/java/ru/krivonogov/springcourse/controllers/FirstController.java
+++ JavaRushTasks/SpringMVCApp1/src/main/java/ru/krivonogov/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.krivonogov.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name1, @RequestParam(value = "surname", required = false) String surname1) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name1, @RequestParam(value = "surname", required = false) String surname1, Model model) {
 
-        System.out.println("Hi, " + name1 + " " + surname1);
+//        System.out.println("Hi, " + name1 + " " + surname1);
+        model.addAttribute("message","Hi, " + name1 + " " + surname1 );
 
         return "first/hello";
     }
