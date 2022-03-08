325814fb496d1f25f243e139d9147d7125d8db64
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-03-08 07:27:16 +0300

    Начал изучать Spring CRUD приложение

--- JavaRushTasks/B.SpringCRUD/src/main/java/ru/alishev/springcourse/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++
@@ -1,2 +0,0 @@
-package ru.alishev.springcourse.config;public class MySpringMvcDispatcherSerlvetIntitializer {
-}
--- JavaRushTasks/B.Spring_MVC_CRUD/src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ JavaRushTasks/B.Spring_MVC_CRUD/src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -1,2 +1,16 @@
-package ru.alishev.springcourse.controllers;public class PeopleController {
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+    @GetMapping()
+    public String index(Model model) {
+        // Получим всех людей из DAO и передадим на отображание в представление
+        return null;
+    }
 }
