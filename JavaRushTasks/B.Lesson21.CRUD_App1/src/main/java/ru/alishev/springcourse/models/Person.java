df386212db3423743ea3caa930ec3c3c271a5459
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-03-20 08:47:40 +0300

    @Valid переварил. Полет нормальный

--- JavaRushTasks/B.Lesson21.CRUD_App1/src/main/java/ru/alishev/springcourse/models/Person.java
+++ JavaRushTasks/B.Lesson21.CRUD_App1/src/main/java/ru/alishev/springcourse/models/Person.java
@@ -1,12 +1,22 @@
 package ru.alishev.springcourse.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 /**
  * @author Neil Alishev
  */
 public class Person {
     private int id;
+    @NotEmpty(message = "Name should not be emtpy")
+    @Size(min = 2, max = 30, message = "Name should by between 2 and 30 characters")
     private String name;
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
+    @NotEmpty(message = "Email should not by emtpy")
+    @Email(message = "Email should by valid")
     private String email;
 
     public Person() {
