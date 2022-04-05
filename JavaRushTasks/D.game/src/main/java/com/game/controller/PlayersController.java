3454ea9e2c7865ea78bccb14283a9c3ade355113
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-04-05 07:53:39 +0300

    Вернулся к тестовому заданию. Сделал заготовку. Завтра продолжу.

---
+++ JavaRushTasks/D.game/src/main/java/com/game/controller/PlayersController.java
@@ -0,0 +1,17 @@
+package com.game.controller;
+
+import com.game.dao.PlayerDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rest")
+public class PlayersController {
+    private final PlayerDAO playerDAO;
+
+    @Autowired
+    public PlayersController(PlayerDAO playerDAO) {
+        this.playerDAO = playerDAO;
+    }
+}
