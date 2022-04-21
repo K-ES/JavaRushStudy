ec548e429c3eb4f6d6523a2a57de5909dc6ef760
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-04-21 07:50:50 +0300

    Завтра продолжу. Окончание сессии.

---
+++ JavaRushTasks/D.game/src/main/java/com/game/entity/Player.java
@@ -0,0 +1,16 @@
+package com.game.entity;
+
+public class Player {
+    public Long id;
+    public String name;
+    public String title;
+    public Race race;
+    public Profession profession;
+    public Long birthday;
+    public Boolean banned;
+    public Integer experience;
+    public Integer level;
+    public Integer untilNextLevel;
+
+
+}
