68be12aa4578647abafd92b37b33eac6e4c525ec
DESKTOP-TGQQQJO\Kes <ekrivonogov@inbox.ru>
2022-04-25 06:54:25 +0300

    2022-04-25 06:53:57 Ае! Победил вставку логирования на пустом проекте по этой инструкции: https://javastudy.ru/log4j/log4j-hello-world-example/

--- JavaRushTasks/G.LoggingLearning/src/main/java/MainClass.java
+++ JavaRushTasks/G.LoggingLearning/src/main/java/MainClass.java
@@ -1,5 +1,17 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MainClass {
+    static final Logger rootLogger = LogManager.getRootLogger();
+    static final Logger userLogger = LogManager.getLogger(MainClass.class);
+
     public static void main(String[] args) {
         System.out.println("Всем привет!");
+
+        userLogger.info("раз");
+        userLogger.info("два");
+
+        rootLogger.info("три");
+
     }
 }
