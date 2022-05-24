cfec9f0e080d6102c3fc0ebd15cfa59661bec436
K-ES <ekrivonogov@inbox.ru>
2022-05-24 07:26:45 +0300

    Проброс на домашний комп

---
+++ JavaRushTasks/D00.GAME/src/main/java/com/game/repository/SearchCriteria.java
@@ -0,0 +1,17 @@
+package com.game.repository;
+
+public class SearchCriteria {
+    private String key;
+    private Object value;
+    private SearchOperation operation;
+
+    public SearchCriteria() {
+    }
+
+    public SearchCriteria(String key, Object value, SearchOperation operation) {
+        this.key = key;
+        this.value = value;
+        this.operation = operation;
+    }
+
+}
